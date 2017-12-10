fde691804e24ecd6ad7f0b7921d3c0201c233613
Prajeesh Prathap <Prajeeshp@infosupport.com>
2017-12-10 20:02:06 +0100

    changed metadata

--- windowsdev/metadata.rb
+++ windowsdev/metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures windowsdev'
 long_description 'Installs/Configures windowsdev'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'windows'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
